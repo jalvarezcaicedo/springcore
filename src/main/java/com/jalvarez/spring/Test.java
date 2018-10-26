4529a27c37384a0a9549aae9bc94a2742c0e7a1f
Jhonatan Alvarez <jhonatan.alvarez@capgemini.com>
2018-10-26 16:07:22 -0500

    Continuacion de curso spring

--- src/main/java/com/jalvarez/spring/Test.java
+++ src/main/java/com/jalvarez/spring/Test.java
@@ -1,13 +1,22 @@
 package com.jalvarez.spring;
 
+import com.jalvarez.spring.list.Hospital;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Test {
 
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
-        Employee emp = (Employee) context.getBean("emp");
-        System.out.println("Employee ID " + emp.getId());
-        System.out.println("Employee Name " + emp.getName());
+        ClassPathXmlApplicationContext classPathContext = new ClassPathXmlApplicationContext("config.xml");
+        Employee employee = (Employee) classPathContext.getBean("employee");
+        System.out.println("Employee ID " + employee.getId());
+        System.out.println("Employee Name " + employee.getName());
+
+        ApplicationContext applicationContext =
+                new ClassPathXmlApplicationContext("listconfig.xml");
+
+        Hospital hospital = (Hospital) applicationContext.getBean("hospital");
+        System.out.println("Hospital Name " + hospital.getName());
+        System.out.println("Hospital Departments " + hospital.getDepartments());
     }
 }
